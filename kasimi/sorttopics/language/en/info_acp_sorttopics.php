5510a7cc7937c738cb4eb72b981e16d905da96e7
kasimi <kasimi@inbox.ru>
2015-12-06 22:43:27 +0100

    Changed quote characters

--- kasimi/sorttopics/language/en/info_acp_sorttopics.php
+++ kasimi/sorttopics/language/en/info_acp_sorttopics.php
@@ -27,9 +27,9 @@ $lang = array_merge($lang, array(
 	'SORTTOPICS_UCP_ENABLED_EXPLAIN'		=> 'Give users the option in the UCP to sort topics by created time in all forums.',
 
 	'SORTTOPICS_SORT_TOPICS_BY'				=> 'Sort topics by',
-	'SORTTOPICS_SORT_TOPICS_BY_EXPLAIN'		=> 'A value other than \'User default\' forces the topics in this forum to be initially sorted by the specified key, disregarding the user\'s sorting preferences in the UCP. The user is still able to temporarily change the sorting at the bottom of each viewforum page.',
+	'SORTTOPICS_SORT_TOPICS_BY_EXPLAIN'		=> 'A value other than “User default” forces the topics in this forum to be initially sorted by the specified key, disregarding the user\'s sorting preferences in the UCP. The user is still able to temporarily change the sorting at the bottom of each viewforum page.',
 	'SORTTOPICS_SORT_TOPICS_ORDER'			=> 'Sort topics order',
-	'SORTTOPICS_SORT_TOPICS_ORDER_EXPLAIN'	=> 'This option is only in effect if the above option is set to a value other than \'User default\'.',
+	'SORTTOPICS_SORT_TOPICS_ORDER_EXPLAIN'	=> 'This option is only in effect if the above option is set to a value other than “User default”.',
 	'SORTTOPICS_USER_DEFAULT'				=> 'User default',
 	'SORTTOPICS_CREATED_TIME'				=> 'Created time',
 ));
