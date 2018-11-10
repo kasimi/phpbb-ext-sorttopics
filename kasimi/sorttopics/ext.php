9220c05c18f0d8e03f80b2f5d63ce3b569f2f19a
kasimi <mail@kasimi.net>
2018-11-10 13:41:01 +0100

    Use version constant PHPBB_VERSION

--- kasimi/sorttopics/ext.php
+++ kasimi/sorttopics/ext.php
@@ -13,14 +13,10 @@ namespace kasimi\sorttopics;
 class ext extends \phpbb\extension\base
 {
 	/**
-	 * Requires phpBB 3.1.4 due to required event data in core.viewforum_get_topic_data
-	 *
 	 * @return bool
-	 * @access public
 	 */
 	public function is_enableable()
 	{
-		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.1.4', '>=');
+		return phpbb_version_compare(PHPBB_VERSION, '3.2.1', '>=');
 	}
 }
